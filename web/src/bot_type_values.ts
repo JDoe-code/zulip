9fcc779c49fed5c3514b3979659f4bdf50cc4755
JDoe-code <isaiahmarte04@gmail.com>
2026-08-05 15:01:28 -0400

    webhooks: Scoping down webhook verification.

--- web/src/bot_type_values.ts
+++ web/src/bot_type_values.ts
@@ -4,5 +4,6 @@ export const INCOMING_WEBHOOK_BOT_TYPE_INT = 2;
 export const OUTGOING_WEBHOOK_BOT_TYPE_INT = 3;
 
 // String forms used as HTML form values.
+export const GENERIC_BOT_TYPE = "1";
 export const OUTGOING_WEBHOOK_BOT_TYPE = "3";
 export const EMBEDDED_BOT_TYPE = "4";
